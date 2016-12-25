4fc443d1c2a07579048e936284bff8e43654d2ad
Muhammad Fauzan <muhifauzan@gmail.com>
2016-12-25 17:36:28 +0700

    Fix destructure structs implementation

--- test/destructure_test.exs
+++ test/destructure_test.exs
@@ -7,7 +7,23 @@ defmodule DestructureTest do
     defstruct [:name, :email]
   end
 
+  defmodule People do
+    defstruct [:people]
+  end
+
   alias __MODULE__.Person
 
   doctest Destructure
+
+  describe "structs" do
+    test "can't match with different structs" do
+      refute d(%People{}) == %Person{}
+    end
+
+    test "can bind struct alias to a variable" do
+      d(%x{}) = %Person{}
+
+      assert x == Person
+    end
+  end
 end
